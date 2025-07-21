0713c0e50e6841a68c88e2b2d3a8398f5ac44f9f
jaepark <parkj7000@icloud.com>
2025-07-20 18:34:48 -0700

    final on js.script

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,14 @@ const newPostClosedBtn = newPostModal.querySelector(".modal__close-btn");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
     editProfileNameInput.value = profileNameEl.textContent;
     editProfileDescriptionInput.value = profileDescriptionEl.textContent;
